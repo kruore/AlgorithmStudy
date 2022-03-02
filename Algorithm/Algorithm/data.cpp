32cfe3503e18185c417130688f224db0b0ae3701
kruore <kruore1321@gmail.com>
2022-03-02 18:37:50 +0900

    20220302
    Solution <1~n까지의 배수의 합>

---
+++ Algorithm/Algorithm/data.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int solution(int n, int m)
+{
+	int sum=0;
+
+	for (int i = 0; i <= n; i+m)
+	{
+		sum += i;
+		i += m;
+	}
+
+	return sum;
+}
+
+
+int main()
+{
+	int data = solution(100, 1);
+	cout << "??????";
+	cout << data;
+}
